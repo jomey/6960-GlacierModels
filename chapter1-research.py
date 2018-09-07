6e83ce5c4f3b8f4a0106998248de9b530426de74
Joachim Meyer <j.meyer@utah.edu>
2018-09-07 06:49:27 -0600

    Move glacier class to dedicated file.
    Also start creating an executable for each class project.

--- chapter1-research.py
+++ chapter1-research.py
@@ -2,62 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-
-class Glacier(object):
-    ICE_DENSITY = 920       # kg/m^3
-    MANTEL_DENSITY = 3200   # kg/m^3
-    GRAVITY = 10
-    SHEER_STRESS = 0.5e5
-
-    ICE_CONSTANT = (2 * SHEER_STRESS) / (ICE_DENSITY * GRAVITY)
-
-    def __init__(self, length, **kwargs):
-        self._length = length
-        self._midpoint = self.length / 2
-        self.mantel_density = kwargs.get('mantel_density', self.MANTEL_DENSITY)
-        self.isostatic = kwargs.get('isostatic', False)
-
-    @property
-    def length(self):
-        return self._length
-
-    @property
-    def midpoint(self):
-        return self._midpoint
-
-    @property
-    def isostatic(self):
-        return self._isostatic
-
-    @isostatic.setter
-    def isostatic(self, value):
-        self._isostatic = value
-
-    @property
-    def mantel_density(self):
-        return self._mantel_density
-
-    @mantel_density.setter
-    def mantel_density(self, value):
-        self._mantel_density = value
-
-    def isostacy(self):
-        return self.ICE_DENSITY / (self.ICE_DENSITY - self.mantel_density)
-
-    def surface_height(self, x):
-        if x >= self.midpoint:
-            x = self.length - x
-
-        height = self.ICE_CONSTANT * x
-
-        if self.isostatic:
-            height = height / (1 + self.isostacy())
-
-        return height ** (1 / 2)
-
-    def bed_depth(self, y):
-        return y * self.isostacy()
-
+from glacier import Glacier
 
 if __name__ == '__main__':
     resolution = 100
