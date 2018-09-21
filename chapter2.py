cc5daabe943976df3cb1e28a143bc9288e3ada4a
Joachim Meyer <j.meyer@utah.edu>
2018-09-20 18:10:19 -0600

    Chapter 2 - Plot out equations 2.1.7 through 2.1.9

--- chapter2.py
+++ chapter2.py
@@ -5,34 +5,74 @@ from glacier import Glacier
 
 if __name__ == '__main__':
     ela_elevations = np.arange(4000, 5100, 100)
+    ela_changes = np.arange(100, 510, 100)
     slopes = np.arange(0.05, 0.30, 0.05)
 
     glacier = Glacier(max_bed_height=5500)
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex='col', figsize=(14,10))
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(
+        nrows=3, ncols=2, figsize=(14, 10)
+    )
 
+    # Length change with varying ELA
     for slope in slopes:
-        lengths = [
+        length_change = [
             glacier.linear_equilibrium_length(elevation, slope)
             for elevation in ela_elevations
         ]
+        ax1.plot(ela_elevations, length_change, label=slope.round(2))
+    ax1.set_title('Length change with varying ELA')
+    ax1.legend(title='Slope')
 
-        ax1.plot(lengths, ela_elevations, label=slope.round(2))
-
-    for elevation in ela_elevations:
+    # Change of length over change of ELA
+    for slope in slopes:
         lengths = [
-            glacier.linear_equilibrium_length(elevation, slope)
+            glacier.length_change(ela_change, slope)
+            for ela_change in ela_changes
+        ]
+        ax3.plot(ela_changes, lengths, label=slope.round(2))
+    ax3.set_title('Length change with varying ELA change')
+    ax3.legend(title='Slope')
+
+    # Fraction of change of length over change of ELA
+    for slope in slopes:
+        lengths = [
+            glacier.fractional_length_change(4500, ela_change, slope)
+            for ela_change in ela_changes
+        ]
+        ax5.plot(ela_changes, lengths, label=slope.round(2))
+    ax5.set_title('Fractional length change with varying ELA change, ELA = 4500 m')
+    ax5.legend(title='Slope')
+
+    # Change of length over varying slope
+    for ela_elevation in ela_elevations:
+        lengths = [
+            glacier.linear_equilibrium_length(ela_elevation, slope)
             for slope in slopes
         ]
+        ax2.plot(slopes, lengths, label=ela_elevation)
+    ax2.set_title('Length change with varying slope')
+    ax2.legend(title='ELA elevation')
 
-        ax2.plot(lengths, slopes, label=elevation, linewidth=.75)
+    # Length change with varying slope
+    for ela_change in ela_changes:
+        lengths = [
+            glacier.length_change(ela_change, slope)
+            for slope in slopes
+        ]
+        ax4.plot(slopes, lengths, label=ela_change)
+    ax4.set_title('Length change with varying slope')
+    ax4.legend(title='ELA change')
 
-    ax1.set_title("Basic Glacier Model on Linear Slope")
-    ax1.set_ylabel("Equilibrium Line Altitude (ELA) (m)")
-    ax1.tick_params(labelbottom=True)
-    ax1.legend(title='Slope Angles')
-    ax2.set_ylabel("Bed Slope")
-    ax2.set_xlabel("Glacier Length (m)")
-    ax2.legend(title='Glacier Elevations')
+    # Fraction of change in length with varying slope
+    # Change in ELA 250m
+    for ela_elevation in ela_elevations:
+        lengths = [
+            glacier.fractional_length_change(ela_elevation, 250, slope)
+            for slope in slopes
+        ]
+        ax6.plot(slopes, lengths, label=ela_elevation)
+    ax6.set_title('Fractional length change with varying slope, ELA change 250')
+    ax6.legend(title='ELA elevation')
+
     plt.show()
-
